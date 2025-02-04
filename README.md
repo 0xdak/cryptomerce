d18058784f57de747d5d9699885258ac89f8fe30
Ali Dak <alidak583@gmail.com>
2025-02-04 23:18:34 +0300

    add buyProduct function

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 **Cryptomerce is a project I'm developing to improve my Solidity skills. It is a smart contract implementation of an e-commerce system.**
 
+
+### TODOs For Development
+
+- [ ] Use the Event type for any variable that can be set
+- [ ] Maybe you want to set the prices in dollars, ETH may +- fastly
+- [ ] implement swap products
+
+### TODOs for Testing
+- [ ] Test buyProduct
--- src/Cryptomerce.sol
+++ src/Cryptomerce.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.0;
 
 /// @title Cryptomerce
 /// @author yuznumara
-/// @notice Just a entrance for Cryptomerce project to complete buy/sell and swap transactions
+/// @notice Just an entrance for Cryptomerce project to complete buy/sell and swap transactions
 contract Cryptomerce {
     // Errors
     error Cryptomerce__NotTheContractOwner();
+    error Cryptomerce__InsufficientValueSent(
+        uint256 sentValue,
+        uint256 requiredValue
+    );
 
+    // @TODO change struct to Event ?
     struct Product {
         uint256 id;
         string name;
@@ -16,6 +21,7 @@ contract Cryptomerce {
 
     address private immutable i_owner;
     Product[] public s_products;
+    mapping(uint256 => address) public s_productIdToOwner;
 
     constructor() {
         i_owner = msg.sender;
@@ -28,5 +34,15 @@ contract Cryptomerce {
 
     function addProduct(string memory name, uint256 price) public {
         s_products.push(Product(s_products.length, name, price));
+    }
+
+    function buyProduct(uint256 productId) public payable {
+        Product memory product = s_products[productId];
+        require(
+            msg.value >= product.price,
+            Cryptomerce__InsufficientValueSent(msg.value, product.price)
+        );
+        payable(s_productIdToOwner[productId]).transfer(msg.value);
+        s_productIdToOwner[productId] = msg.sender;
     }
 }
